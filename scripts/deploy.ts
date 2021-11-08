f8e0f35841c2f67a590dd5dffd4abea3767baffe
kyzooghost <73516204+kyzooghost@users.noreply.github.com>
2021-11-08 19:06:37 +1100

    Successful programmatic Etherscan contract verification

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,6 +4,7 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
+const hre = require("hardhat");
 
 const RINKEBY_VRF_COORDINATOR = "0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B";
 const RINKEBY_LINKTOKEN = "0x01be23585060835e02b77ef475b0cc51aa1e0709";
@@ -17,7 +18,6 @@ async function main() {
   // manually to make sure everything is compiled
   // await hre.run('compile');
 
-  // We get the contract to deploy
   const CovidCats = await ethers.getContractFactory("CovidCats");
   
   const covidCats = await CovidCats.deploy(
@@ -28,6 +28,20 @@ async function main() {
 
   await covidCats.deployed();
   console.log("COVIDNFT contract deployed to:", covidCats.address);
+  
+  // Wait 5 blocks after contract deploy transactions to be safe to avoid Etherscan error 
+  // where you are asking for contract verification before the contract is indexed on Etherscan
+  await covidCats.deployTransaction.wait(5)
+
+  // Verify the contract on Etherscan
+  await hre.run("verify:verify", {
+    address: covidCats.address,
+    constructorArguments: [
+      RINKEBY_VRF_COORDINATOR,
+      RINKEBY_LINKTOKEN,
+      RINKEBY_KEYHASH
+    ]
+  });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
